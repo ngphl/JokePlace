8127fc789c2f230873494acbcd6370c634e9d690
Long Nguyen <npl.long2001@gmail.com>
2023-06-09 03:01:11 +1000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokePlace.Models;
+
+namespace JokePlace.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<JokePlace.Models.Joke> Joke { get; set; } = default!;
+	}
+}
